2379886dc46538563e3619f65abed9535add6cc1
mohamed22ahmed <mohammedahmedhammam113@gmail.com>
2021-07-27 05:53:31 +0200

    installing nue packages and laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,40 @@
 require('./bootstrap');
+window.Vue = require("vue");
+
+import App from "./App.vue";
+import axios from "axios";
+import { routes } from "./routes";
+import VueRouter from "vue-router";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, axios);
+Vue.use(VueRouter);
+
+import { Form, HasError, AlertError } from "vform";
+window.Fire = new Vue();
+
+import swal from "sweetalert2";
+
+window.swal = swal;
+
+import VueProgressBar from "vue-progressbar";
+
+Vue.use(VueProgressBar, {
+    color: "rgb(143, 255, 199)",
+    failedColor: "red",
+    height: "3px"
+});
+
+window.Form = Form;
+Vue.component(HasError.name, HasError);
+Vue.component(AlertError.name, AlertError);
+
+const router = new VueRouter({
+    mode: "history",
+    routes: routes
+});
+
+const app = new Vue({
+    el: "#app",
+    router: router,
+    render: h => h(App)
+});
---
+++ resources/js/routes.js
@@ -0,0 +1,7 @@
+import test_form from "./components/test_form.vue";
+
+export const routes = [{
+    name: "home",
+    path: "/",
+    component: test_form
+}];
