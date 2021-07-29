f29b79e3af4b6c57e5ce662ffb8477108015f345
mohamed22ahmed <mohammedahmedhammam113@gmail.com>
2021-07-29 09:07:46 +0200

    installing barryvdh/laravel-dompdf package to generate pdf with users data

--- app/Console/Commands/ExportData.php
+++ app/Console/Commands/ExportData.php
@@ -8,6 +8,8 @@ use App\Mail\Sending_users;
 use App\Mail\Confirm_user;
 use App\User;
 use Storage;
+use PDF;
+
 class ExportData extends Command
 {
     /**
@@ -41,26 +43,62 @@ class ExportData extends Command
      */
     public function handle()
     {
-        $users=User::where('admin_sent',0)->get();
-        foreach($users as $user){
+        $users = User::where('admin_sent', 0)->get();
+        foreach ($users as $user) {
             // sending message to Admin to inform him that he recieved a new request
-            $data=[
-                'name'=>$user->name,
-                'name_ar'=>$user->name_ar,
-                'email'=>$user->email,
-                'brand_name'=>$user->brand_name,
-                'brand_name_ar'=>$user->brand_name_ar
+            $data = [
+                'name' => $user->name,
+                'name_ar' => $user->name_ar,
+                'email' => $user->email,
+                'brand_name' => $user->brand_name,
+                'brand_name_ar' => $user->brand_name_ar
             ];
-            $mail=Mail::to('mmm619253@gmail.com')->send(new Sending_users($data));
+            $mail = Mail::to('mmm619253@gmail.com')->send(new Sending_users($data));
 
             // sending message to user to inform him that we recieved his request
-            $data = ['name'=>$user->name,'confirm' => 'We have received your request'];
+            $data = ['name' => $user->name, 'confirm' => 'We have received your request'];
             Mail::to($user->email)->send(new Confirm_user($data));
 
             // changing the status for this request to don't send again to mail
-            $user->update(['admin_sent'=>1]);
+            $user->update(['admin_sent' => 1]);
         }
 
         $this->info('Successful');
     }
+
+    // function pdf()
+    // {
+    //     $pdf = \App::make('dompdf.wrapper');
+    //     $pdf->loadHTML($this->convert_users_data_to_html());
+    //     return $pdf->stream();
+    // }
+
+    // function convert_users_data_to_html()
+    // {
+    //     $customer_data = $this->get_customer_data();
+    //     $output = '
+    //             <h3 align="center">New Requests</h3>
+    //             <table width="100%" style="border-collapse: collapse; border: 0px;">
+    //             <tr>
+    //             <th style="border: 1px solid; padding:12px;" width="20%">Name</th>
+    //             <th style="border: 1px solid; padding:12px;" width="30%">Arabic Name</th>
+    //             <th style="border: 1px solid; padding:12px;" width="15%">Email</th>
+    //             <th style="border: 1px solid; padding:12px;" width="15%">Brand Name</th>
+    //             <th style="border: 1px solid; padding:12px;" width="20%">Arabic Brand Name</th>
+    //         </tr>
+    //     ';
+    //     foreach ($customer_data as $customer) {
+    //         $output .= '
+    //                 <tr>
+    //                 <td style="border: 1px solid; padding:12px;">' . $customer->CustomerName . '</td>
+    //                 <td style="border: 1px solid; padding:12px;">' . $customer->Address . '</td>
+    //                 <td style="border: 1px solid; padding:12px;">' . $customer->City . '</td>
+    //                 <td style="border: 1px solid; padding:12px;">' . $customer->PostalCode . '</td>
+    //                 <td style="border: 1px solid; padding:12px;">' . $customer->Country . '</td>
+    //                 </tr>
+    //             ';
+    //     }
+    //     $output .= '</table>';
+    //     return $output;
+    // }
 }
